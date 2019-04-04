86b4262e58b55a2852c1715869018cab87fc63d2
inventivetalent <mail@inventivetalent.org>
2019-04-04 23:40:27 +0200

    server base copy

--- src/main/resources/templates/containerInitScript.sh
+++ src/main/resources/templates/containerInitScript.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 CONTAINER_VERSION="--CONTAINER_VERSION--"
 
+
+echo "Please specify the source directory to use as the server base (leave blank to skip):"
+read serverBase
+
+if [[ ! -z $serverBase ]]; then
+    echo "Copying files from $serverBase..."
+    cp -a -n serverBase .
+fi
+
+
 echo "Downloading container plugin..."
 wget -O plugins/MineTileContainer.jar https://github.com/InventivetalentDev/MineTileContainer/releases/download/$CONTAINER_VERSION/container-$CONTAINER_VERSION.jar"
 
